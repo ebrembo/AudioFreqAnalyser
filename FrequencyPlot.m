b83d57e26d56e4802798035b73bbdf967dbb5b8a
Unknown <ebrembo@gmail.com>
2017-04-14 09:40:02 +0200

    Initial matlab script upload

---
+++ FrequencyPlot.m
@@ -0,0 +1,47 @@
+%% clear old data
+clear all; close all; clc
+
+%% Select and open audio file
+[FileName, PathName]= uigetfile('*') ;
+File = [PathName, '\' , FileName];
+[data, fs] = audioread(File);
+
+
+% %%
+% X1 = fftshift( data(:, 1));
+% X2 = fftshift( data(:, 2));
+
+%%
+L = length(data);
+NFFT = 2^nextpow2(L); % Next power of 2 from length of y
+
+Y1 = fft(data(:,1),NFFT)/L;
+X1 = fs/2*linspace(0,1,NFFT/2+1);
+
+Y2 = fft(data(:,2),NFFT)/L;
+X2 = fs/2*linspace(0,1,NFFT/2+1);
+
+% Plot single-sided amplitude spectrum.
+figure
+subplot(2,1,1)
+title(FileName)
+    loglog(X1,2*abs(Y1(1:NFFT/2+1)))    
+    xlabel('Frequency (Hz)')
+    ylabel('|Y(f)|')
+    set(gca, 'XLim', [5 max(get(gca,'xlim'))]);
+subplot(2,1,2)
+    loglog(X2,2*abs(Y2(1:NFFT/2+1))) 
+    xlabel('Frequency (Hz)')
+    ylabel('|Y(f)|')
+    set(gca, 'XLim', [5 max(get(gca,'xlim'))]);
+% title('Single-Sided Amplitude Spectrum of y(t)')
+
+%% Save emf plot in the same location as the music file
+if File(end-3) == '.'
+    saveas(gcf, [ File(1:end-3) , 'emf'])
+elseif File(end-4) == '.'
+     saveas(gcf, [ File(1:end-4) , 'emf'])
+end
+
+% close graph
+close gcf
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 AudioFreqAnalyser
+
+This matlab tool is used to open audio files and analyse the spectum of the sound
